6aa7d09b585c6428d739ecc5be24c442de6b1be3
wforney <wforney@hotmail.com>
2014-11-25 00:36:57 -0800

    build fix

--- src/WebApp/Gruntfile.js
+++ src/WebApp/Gruntfile.js
@@ -4,122 +4,10 @@ This file in the main entry point for defining grunt tasks and using grunt plugi
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
 */
 module.exports = function (grunt) {
+    require("time-grunt")(grunt);
+
     grunt.initConfig({
-        JS_FILES: "wwwroot/js/**/*.js",
-        staticFilePattern: "**/*.{js,css,map,html,htm,ico,jpg,jpeg,png,gif,eot,svg,ttf,woff}",
-        pkg: grunt.file.readJSON("package.json"),
-        bower: {
-            install: {
-                options: {
-                    targetDir: "wwwroot/lib",
-                    layout: "byComponent",
-                    cleanTargetDir: true
-                }
-            }
-        },
-        clean: {
-            build: [
-                "wwwroot/index.html",
-                "wwwroot/css/**/*.css",
-                "wwwroot/js/**/*.js"
-            ],
-            postbuild: [
-                "wwwroot/js/app.templates*.js"
-            ]
-        },
-        copy: {
-            build: {
-                src: "index.html",
-                dest: "wwwroot/"
-            }
-        },
-        concat: {
-            build: {}
-        },
-        cssmin: {
-            build: {}
-        },
-        filerev: {
-            options: {
-                algorithm: "md5",
-                encoding: "utf8",
-                length: 20
-            },
-            build: {
-                src: [
-                    "wwwroot/img/**/*.{gif,jpg,jpeg,png,webp,svg}",
-                    "<%= JS_FILES %>",
-                    "wwwroot/css/**/*.css",
-                    "wwwroot/lib/**/*.{js,css,eot,svg,ttf,woff}"
-                ]
-            }
-        },
-        htmlmin: {
-            build: {
-                options: {
-                    removeComments: true,
-                    collapseWhitespace: true
-                },
-                files: {
-                    "wwwroot/index.html": "wwwroot/index.html"
-                }
-            }
-        },
-        jshint: {
-            files: ["<%= JS_FILES %>"],
-            options: {
-                es5: true,
-                laxcomma: true
-            }
-        },
-        less: {
-            options: {
-                compress: true,
-                yuicompress: true,
-                optimization: 2,
-                banner: "/*! <%= pkg.name %> <%= grunt.template.today(\"yyyy-mm-dd\") %> */\n",
-                paths: ["less"]
-            },
-            files: {
-                expand: true,
-                cwd: "less/",
-                src: ["site.less"],
-                dest: "wwwroot/css/",
-                ext: ".css"
-            }
-        },
-        ngtemplates: {
-            app: {
-                src: "app/**/*.html",
-                dest: "wwwroot/js/app.templates.js",
-                options: {
-                    base: "wwwroot/"
-                }
-            }
-        },
-        uglify: {
-            options: {
-                banner: "/*! <%= pkg.name %> <%= grunt.template.today(\"yyyy-mm-dd\") %> */\n"
-            }
-        },
-        usemin: {
-            html: ["wwwroot/index.html"],
-            css: ["wwwroot/**/*.css"],
-            options: {
-                blockReplacements: {
-                    less: function (block) {
-                        return "<link rel=\"stylesheet\" href=\"" + block.dest + "\">";
-                    }
-                }
-            }
-        },
-        useminPrepare: {
-            html: ["wwwroot/index.html"],
-            options: {
-                dest: "wwwroot",
-                root: "wwwroot"
-            }
-        }
+        pkg: grunt.file.readJSON("package.json")
     });
 
     grunt.registerTask("ts", [
@@ -134,8 +22,8 @@ module.exports = function (grunt) {
         "clean:build",
         "bower:install",
         "copy",
-        "ts",
         "jshint",
+        "ts",
         "ngtemplates",
         "less",
         "useminPrepare",
@@ -150,4 +38,6 @@ module.exports = function (grunt) {
 
     // ReSharper disable once UseOfImplicitGlobalInFunctionScope
     require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+
+    require("load-grunt-config")(grunt);
 };
