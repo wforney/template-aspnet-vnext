d8f518277857ff191c423c5972e74cff7f4c2f39
wforney <wforney@hotmail.com>
2014-11-26 09:56:43 -0800

    Fixed Angular setup.

--- src/WebApp/app/app.ts
+++ src/WebApp/app/app.ts
@@ -1,25 +1,19 @@
 ﻿/// <reference path="../ng-apps/references.ts" />
 /// <reference path="../typings/angularjs/angular.d.ts" />
+/// <reference path="../typings/angularjs/angular-resource.d.ts" />
 /// <reference path="../typings/angularjs/angular-route.d.ts" />
-var dependencies = [
-    "ngResource",
-    "ngRoute"
-];
+angular.module(
+    "app", [
+        "ngResource",
+        "ngRoute"
+    ]).config([
+        "$routeProvider",
+        "$logProvider",
+        ($routeProvider: ng.route.IRouteProvider, $logProvider: ng.ILogProvider) => {
+            $logProvider.debugEnabled(true);
 
-var app = angular.module("app", dependencies);
-angular.bootstrap(document, ["app"]);
-
-app.config(($routeProvider: ng.route.IRouteProvider, $logProvider: ng.ILogProvider) => {
-    $logProvider.debugEnabled(true);
-
-    $routeProvider
-        .when("/", { templateUrl: "views/home.html", controller: "home" })
-        .otherwise({ redirectTo: "/" });
-});
-
-app.config([
-    "$httpProvider", $httpProvider => {
-        $httpProvider.defaults.useXDomain = true;
-        delete $httpProvider.defaults.headers.common["X-Requested-With"];
-    }
-]);
+            $routeProvider
+                .when("/", { templateUrl: "views/home.html", controller: "home" })
+                .otherwise({ redirectTo: "/" });
+        }
+    ]);
