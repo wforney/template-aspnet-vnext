a121a4fb331049e8adcd44e4baa01bdaa19cfd43
wforney <wforney@hotmail.com>
2014-11-26 21:40:17 -0800

    Sync up

--- src/WebApp/Angular/AngularServerExtension.cs
+++ src/WebApp/Angular/AngularServerExtension.cs
@@ -9,21 +9,22 @@
 
     public static class AngularServerExtension
     {
-        ////public static IApplicationBuilder UseAngularServer(this IApplicationBuilder builder, string rootPath, string entryPath)
-        ////{
-        ////    var options = new AngularServerOptions()
-        ////    {
-        ////        FileServerOptions = new FileServerOptions()
-        ////        {
-        ////            EnableDirectoryBrowsing = false,
-        ////            FileSystem = new PhysicalFileSystem(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, rootPath))
-        ////        },
-        ////        EntryPath = new PathString(entryPath)
-        ////    };
+        public static IApplicationBuilder UseAngularServer(this IApplicationBuilder builder, string rootPath, string entryPath)
+        {
+            var options = new AngularServerOptions()
+            {
+                FileServerOptions = new FileServerOptions()
+                {
+                    EnableDirectoryBrowsing = false,
+                    // TODO: Fix for Core CLR - AppDomain isn't available.
+                    ////            FileSystem = new PhysicalFileSystem(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, rootPath))
+                },
+                EntryPath = new PathString(entryPath)
+            };
 
-        ////    builder.UseDefaultFiles(options.FileServerOptions.DefaultFilesOptions);
+            builder.UseDefaultFiles(options.FileServerOptions.DefaultFilesOptions);
 
-        ////    return builder.Use(next => new AngularServerMiddleware(next, env, options).Invoke);
-        ////}
+            return builder.Use(next => new AngularServerMiddleware(next, env, options).Invoke);
+        }
     }
 }
